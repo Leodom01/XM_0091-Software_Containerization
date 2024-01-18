a69a8d7749b00bdd502f648389eff60a93e46c76
Leodom01 <leonardodominini01@gmail.com>
2024-01-18 17:24:08 +0000

    Self initializing mongodb container

--- dbTier/initDb.sh
+++ dbTier/initDb.sh
@@ -1,3 +1,25 @@
-mongoimport --db appDb --collection users --file /app/users.json --jsonArray
-mongoimport --db appDb --collection reminders --file /app/reminders.json --jsonArray
-mongoimport --db appDb --collection roles --file /app/roles.json --jsonArray
+#!/bin/bash
+need_init=0
+
+if [ $(mongosh localhost:27017 --eval 'db.getMongo().getDBNames().indexOf("appDb")' --quiet) -lt 0 ] || \
+   [ $(mongosh localhost:27017/appDb --eval 'db.getCollectionNames().indexOf("users")' --quiet) -lt 0 ] || \
+   [ $(mongosh localhost:27017/appDb --eval 'db.getCollectionNames().indexOf("reminders")' --quiet) -lt 0 ]; then
+    
+    need_init=1
+    echo "I am gonna initialize the db..."
+    # Drop the whole databases in case some collections, but not all of them
+    mongosh localhost:27017 --eval 'db.dropDatabase("appDb")' --quiet
+else 
+    need_init=0
+    echo "Db already initialized"
+fi
+
+if [ "$need_init" -eq "1" ]; then
+    mongosh localhost:27017 --eval 'use appDb' --quiet
+    mongoimport --db appDb --collection users --file /app/users.json --jsonArray
+    mongoimport --db appDb --collection reminders --file /app/reminders.json --jsonArray
+
+    echo "Db initialized!"
+fi
+
+echo "Initialization routine finished"
