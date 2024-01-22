6c2f3939b23b20ce0df1a6ab6c3aa3292dd0b7f9
Minh Duc Nguyen <nguyenminhducci@gmail.com>
2024-01-22 21:41:50 +0100

    scale frontend replicas down

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,8 +4,5 @@ import router from './router'
 import store from './store'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import dotenv from 'dotenv'
-
-// dotenv.config()
 
 createApp(App).use(router).use(store).mount('#app')
