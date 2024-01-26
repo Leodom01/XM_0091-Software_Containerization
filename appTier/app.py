ca317d1357c901ddf050ef61ac224a7bcc3b9df5
dalviebenu <dalvie.benu@gmail.com>
2024-01-26 18:20:32 +0100

    added logging

--- appTier/app.py
+++ appTier/app.py
@@ -4,12 +4,30 @@ from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
 import secrets
 from gevent.pywsgi import WSGIServer
+from logging.config import dictConfig
 
 app = Flask(__name__)
 
+# Logging Config
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
 # MongoDB Config
 # TODO: Replace with our MongoDB URI
-app.config["MONGO_URI"] = "mongodb://your_mongodb_uri"  # Replace with your MongoDB URI
+app.config["MONGO_URI"] = "db-svc.default.svc.cluster.local:27017"  # URI from mongodb container
 mongo = PyMongo(app)
 
 # JWT Config with random key
@@ -21,6 +39,7 @@ jwt = JWTManager(app)
 # User Authentication Routes
 @app.route('/register', methods=['POST'])
 def register():
+    app.logger.info('Registering user')
     username = request.json.get('username')
     password = request.json.get('password')
     hashed_password = generate_password_hash(password)
@@ -31,6 +50,7 @@ def register():
 @app.route('/login', methods=['POST'])
 def login():
     user = mongo.db.users.find_one({'username': request.json.get('username')})
+    app.logger.info('user login')
     if user and check_password_hash(user['password'], request.json.get('password')):
         access_token = create_access_token(identity=user['username'])
         session['jwt'] = access_token
@@ -42,6 +62,7 @@ def login():
 @app.route('/reminders', methods=['GET'])
 @jwt_required()
 def get_reminders():
+    app.logger.info('User request reminders')
     reminders = mongo.db.reminders.find()
     return jsonify([reminder for reminder in reminders])
 
@@ -49,6 +70,7 @@ def get_reminders():
 @app.route('/reminders', methods=['POST'])
 @jwt_required()
 def add_reminder():
+    app.logger.info('user added reminder')
     reminder = request.json
     mongo.db.reminders.insert_one(reminder)
     return jsonify(reminder), 201
