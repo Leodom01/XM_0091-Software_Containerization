4b0269a2f8e0cb204bca33038443104419930ba8
Leodom01 <leonardodominini01@gmail.com>
2024-01-30 15:59:48 +0000

    PVC included in dbTier statefulset+major debug

--- dbTier/initDb.sh
+++ dbTier/initDb.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 need_init=0
 
+echo "Running the init script"
+
 if [ $(mongosh localhost:27017 --eval 'db.getMongo().getDBNames().indexOf("appDb")' --quiet) -lt 0 ] || \
    [ $(mongosh localhost:27017/appDb --eval 'db.getCollectionNames().indexOf("users")' --quiet) -lt 0 ] || \
    [ $(mongosh localhost:27017/appDb --eval 'db.getCollectionNames().indexOf("reminders")' --quiet) -lt 0 ]; then
@@ -15,6 +17,7 @@ else
 fi
 
 if [ "$need_init" -eq "1" ]; then
+    echo "Initialization procedure started..."
     mongosh localhost:27017 --eval 'use appDb' --quiet
     mongoimport --db appDb --collection users --file /app/users.json --jsonArray
     mongoimport --db appDb --collection reminders --file /app/reminders.json --jsonArray
